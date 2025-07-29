5b41c539fd94ac2b4a0cfd8eb308d2a69650431a
kakahikari <kakahikari@gmail.com>
2025-07-29 16:37:26 +0800

    feat: 年份改為當前年份 & vitepress版本號來自 package.json

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.0.2</a>
+        MIT Licensed | Copyright © {{ currentYear }} <a class="vitepress" :href="website">{{ webTitle }}</a><br />
+        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - {{ vitepressVersion }}</a>
         Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
     </div>
 </template>
 <script lang="ts" setup>
 import { useData } from 'vitepress'
+import packageJson from '../../../package.json'
 
 const { site, theme } = useData()
 const website = theme.value.website
 const webTitle = site.value.title
+const vitepressVersion = packageJson.devDependencies.vitepress.replace('^', '')
+const currentYear = new Date().getFullYear()
 </script>
 
 <style>
