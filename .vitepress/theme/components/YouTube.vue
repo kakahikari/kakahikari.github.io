789e4d72bbe9384b7b36161928f482f13e7aaf69
kakahikari <kakahikari@gmail.com>
2025-09-03 14:29:30 +0800

    feat: components/YouTube 維持16:9比例

--- .vitepress/theme/components/YouTube.vue
+++ .vitepress/theme/components/YouTube.vue
@@ -1,8 +1,6 @@
 <template>
   <div v-if="url" class="video-responsive">
     <iframe
-      width="100%"
-      height="400"
       :src="url"
       frameborder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -21,6 +19,21 @@ const props = defineProps<{
 const url = computed(() => `https://www.youtube.com/embed/${props.id}`)
 </script>
 
-<style>
-/* TODO: 播放器RWD */
+<style scoped>
+.video-responsive {
+  position: relative;
+  height: 0;
+  /* 16:9 */
+  padding-bottom: 56.25%;
+  overflow: hidden;
+}
+
+.video-responsive iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
 </style>
