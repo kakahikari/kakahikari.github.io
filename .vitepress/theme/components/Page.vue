9bf58c7d3190861408a38d7824a284c985b128bf
kakahikari <kakahikari@gmail.com>
2026-03-19 10:15:42 +0800

    fix: 移除visually-hidden內容

--- .vitepress/theme/components/Page.vue
+++ .vitepress/theme/components/Page.vue
@@ -1,8 +1,4 @@
 <template>
-  <div v-if="pageCurrent === 1" class="visually-hidden">
-    <h1>{{ site.title }}</h1>
-    <p>{{ site.description }}</p>
-  </div>
   <template v-for="(post, index) in posts" :key="index">
     <div class="post">
       <PostCard :post="post" />
@@ -18,11 +14,7 @@
 </template>
 
 <script lang="ts" setup>
-import { useData } from 'vitepress'
-
 import type { Post } from '../types'
-
-const { site } = useData()
 
 defineProps<{
   posts: Post[]
@@ -38,18 +30,6 @@ const getPageUrl = (pageNum: number): string => {
 <style scoped>
 .post {
   margin: calc(var(--block-margin) * 1.5) 0;
-}
-
-.visually-hidden {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
-  white-space: nowrap;
 }
 
 /* TODO: 媒體查詢無法使用變數的替代方案 */
